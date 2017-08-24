ad02f84e0264953cf7c0d7f82eb39ab82ebe01a4
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-08-24 14:26:12 +0200

    Add proxy test

---
+++ proxy/proxy_test.go
@@ -0,0 +1,25 @@
+package proxy
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/frozzare/alfred/internal/docker"
+	"github.com/frozzare/go-assert"
+)
+
+func TestApp(t *testing.T) {
+	d, err := docker.NewDocker()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	proxy := NewProxy(&Options{
+		Docker: d,
+		Name:   "alfred_proxy",
+	})
+
+	assert.Nil(t, proxy.Start())
+	assert.True(t, strings.Contains(proxy.Start().Error(), "container already exists"))
+	assert.Nil(t, proxy.Stop())
+}
