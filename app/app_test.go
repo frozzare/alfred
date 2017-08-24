dfc57962298e7cd42d3570e3379b365e78c2fd7c
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-08-24 13:02:13 +0200

    Fix test case

--- app/app_test.go
+++ app/app_test.go
@@ -4,14 +4,20 @@ import (
 	"testing"
 
 	"github.com/frozzare/alfred/config"
+	"github.com/frozzare/alfred/internal/docker"
 	"github.com/frozzare/go-assert"
 )
 
 func TestApp(t *testing.T) {
 	path := "../examples/html/alfred.json"
 	config, _ := config.ReadConfig(path)
+	d, err := docker.NewDocker()
+	if err != nil {
+		t.Fatal(err)
+	}
 	app := NewApp(&Options{
 		Config: config,
+		Docker: d,
 	})
 
 	assert.Nil(t, app.Start())
